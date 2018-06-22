a97fbb41dc3b46f18d8af31149cb51f10866142e
shunfski <fengshun_ios@126.com>
2018-06-22 14:00:29 +0800

    优化

---
+++ QQtagView/CustomBtn.h
@@ -0,0 +1,13 @@
+//
+//  CustomBtn.h
+//  QQtagView
+//
+//  Created by qinmuqiao on 2018/6/22.
+//  Copyright © 2018年 ZhangQun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomBtn : UIButton
+@property (nonatomic , strong) id  info;
+@end
---
+++ QQtagView/CustomBtn.m
@@ -0,0 +1,21 @@
+//
+//  CustomBtn.m
+//  QQtagView
+//
+//  Created by qinmuqiao on 2018/6/22.
+//  Copyright © 2018年 ZhangQun. All rights reserved.
+//
+
+#import "CustomBtn.h"
+
+@implementation CustomBtn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
